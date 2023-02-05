c1973190fbc19625c292cacb1ce50d49404a7ead
Tamagaft <ihorvoloshanovich@gmail.com>
2023-02-05 13:07:54 +0200

    fixed typo

--- internals/entity/user.go
+++ internals/entity/user.go
@@ -1,7 +1,7 @@
 package entity
 
 type User struct {
-	Id          int    `db:"is" json:"id"`
+	Id          int    `db:"id" json:"id"`
 	Username    string `db:"username" json:"username"`
 	Description string `db:"description" json:"description"`
 	Password    string `db:"password" json:"password"`
